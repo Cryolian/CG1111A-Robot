229073eab63d16a84fe2b873799580c3679436f0
Cryolian <102575848+Cryolian@users.noreply.github.com>
2022-10-20 09:18:24 +0800

    Add files via upload

--- CG1111A/CG1111A.ino
+++ CG1111A/CG1111A.ino
@@ -1,68 +1,233 @@
 #include "MeMCore.h"
 #include <MeMCore.h>
 
+//Pin Definitions
+#define ULTRASONIC_PIN 12 //12 for port 1, 10 for port 2
+#define IR_RECEIVER_PIN A0
+#define LDR_PIN A1
+#define DECODER_B A2  //connect decoder connection to port 3
+#define DECODER_A A3
+
+//Value Definitions
+#define SPEED_OF_SOUND 345
 #define TURNING_TIME_MS 330 // The time duration (ms) for turning
+#define LDR_WAIT 15
+#define CORRECT_TIMES 2
+#define RGB_TIME 200
+
 
 MeBuzzer buzzer; // create the buzzer object
 MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
 MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
-MeRGBLed led(0,30); // Based on hardware connections on mCore; cannot change
+MeRGBLed led(0, 30); // Based on hardware connections on mCore; cannot change
+MeLineFollower lineFinder(PORT_2);
+
 
 uint8_t motorSpeed = 255;
 // Setting motor speed to an integer between 1 and 255
 // The larger the number, the faster the speed
 
-void celebrate() {
-  // Each of the following "function calls" plays a single tone.
-  // The numbers in the bracket specify the frequency and the duration (ms)
-  buzzer.tone(392, 200);
-  buzzer.tone(523, 200);
-  buzzer.tone(659, 200);
-  buzzer.tone(784, 200);
-  buzzer.tone(659, 150);
-  buzzer.tone(784, 400);
-  buzzer.noTone();
+int status = 0;
+int red = 0;
+int green = 0;
+int blue = 0;
+
+float colourArray[] = {0, 0, 0};
+float whiteArray[] = {0, 0, 0};
+float blackArray[] = {0, 0, 0};
+float greyDiff[] = {0, 0, 0};
+char colourStr[3][5] = {"R = ", "G = ", "B = "};
+
+void stop_motor() {
+  leftMotor.stop();
+  rightMotor.stop();
 }
 
-void light_show() {
-  led.setColor(255, 255, 255); // set both LEDs to white colour
-  led.show(); // Must use .show() to make new colour take effect
-  delay(500);
-  led.setColorAt(0, 255, 0, 0); // set Right LED to Red
-  led.setColorAt(1, 0, 0, 255); // set Left LED to Blue
-  led.show();
-  delay(500);
-  led.setColorAt(0, 0, 0, 255); // set Right LED to Blue
-  led.setColorAt(1, 255, 0, 0); // set Left LED to Red
-  led.show();
-  delay(500);
+void go_forward() {
+  leftMotor.run(-motorSpeed);
+  rightMotor.run(motorSpeed);
+}
+
+void reverse(int time) {
+  leftMotor.run(motorSpeed);
+  rightMotor.run(-motorSpeed);
+  delay(time);
+  stop_motor();
+}
+
+void right_turn(int time) {
+  leftMotor.run(-motorSpeed);
+  rightMotor.run(-motorSpeed);
+  delay(time);
+  stop_motor();
+}
+
+void left_turn(int time) {
+  leftMotor.run(motorSpeed);
+  rightMotor.run(motorSpeed);
+  delay(time);
+  stop_motor();
+}
+
+void right_turn_2_grid(int turn_time, int fwd_time) {
+  right_turn(turn_time);
+  stop_motor();
+  go_forward();
+  delay(fwd_time);
+  stop_motor();
+  right_turn(turn_time);
+  stop_motor();
+}
+
+void left_turn_2_grid(int turn_time, int fwd_time) {
+  left_turn(turn_time);
+  go_forward();
+  delay(fwd_time);
+  stop_motor();
+  left_turn(turn_time);
+}
+
+void rotate_back(int time) {
+  right_turn(time);
+  right_turn(time);
+  stop_motor();
+}
+
+void calibrate_speed_of_sound() {
+  float speed;
+  Serial.println("Place object 10 cm away from sensor");
+
+  delay(2000);
+  digitalWrite(ULTRASONIC_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_PIN, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(ULTRASONIC_PIN, LOW);
+
+  pinMode(ULTRASONIC_PIN, INPUT);
+  long duration = pulseIn(ULTRASONIC_PIN, HIGH);
+  Serial.println(duration);
+
+  speed = ((10.0 * 2 * 10000.0) / ((float)duration ));
+  Serial.println(speed);
+}
+
+float distance_left() {
+  digitalWrite(ULTRASONIC_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC_PIN, HIGH);
+  delayMicroseconds(100);
+  digitalWrite(ULTRASONIC_PIN, LOW);
+
+  pinMode(ULTRASONIC_PIN, INPUT);
+  long duration = pulseIn(ULTRASONIC_PIN, HIGH);
+  float distance = (((float)SPEED_OF_SOUND * (float)duration / 10000.0) / 2.0) - 3.5;
+  //  Serial.println(duration);
+  //  Serial.print("distance: ");
+  //  Serial.println(distance);
+  //  Serial.println("cm");
+  delay(300);
+  return distance;
+}
+
+void adjust_angle(float difference) {
+  float distance_1 = distance_left();
+  go_forward();
+  delay(250);
+  stop_motor();
+  float distance_2 = distance_left();
+
+  if (distance_2 > distance_1) {
+
+    while ((distance_2 - distance_1) > difference) {
+      left_turn(35);
+      stop_motor();
+      distance_2 = distance_left();
+    }
+  }
+
+  else if (distance_2 < distance_1) {
+
+    while ((distance_1 - distance_2) > difference) {
+      right_turn(35);
+      stop_motor();
+      distance_2 = distance_left();
+    }
+  }
+}
+
+void correct_path() {
+  for (int i = 0; i < CORRECT_TIMES; i += 1) {
+    adjust_angle(0.00);
+    reverse(200);
+    delay(1000);
+  }
+}
+
+void shine_red(int time) {
+  analogWrite(DECODER_A, 255);
+  analogWrite(DECODER_B, 255);
+  delay(time);
+  analogWrite(DECODER_A, LOW);
+  analogWrite(DECODER_B, LOW);
+  Serial.print("Red: ");
+  Serial.println(analogRead(LDR_PIN));
+  delay(LDR_WAIT);
+}
+
+void shine_green(int time) {
+  analogWrite(DECODER_A, 255);
+  analogWrite(DECODER_B, 0);
+  delay(time);
+  analogWrite(DECODER_A, LOW);
+  analogWrite(DECODER_B, LOW);
+  Serial.print("Green: ");
+  Serial.println(analogRead(LDR_PIN));
+  delay(LDR_WAIT);
+}
+
+void shine_blue(int time) {
+  analogWrite(DECODER_A, 0);
+  analogWrite(DECODER_B, 255);
+  delay(time);
+  analogWrite(DECODER_A, LOW);
+  analogWrite(DECODER_B, LOW);
+  Serial.print("Blue: ");
+  Serial.println(analogRead(LDR_PIN));
+  delay(LDR_WAIT);
 }
 
 void setup() {
-  // Any setup code here runs only once:
-  delay(1000); // Do nothing for 10000 ms = 10 seconds
-  celebrate();
-  led.setpin(13);
+
+  delay(2000); // Do nothing for 10000 ms = 10 seconds
+  Serial.begin(9600);
+  //  correct_path();
+  pinMode(DECODER_A, OUTPUT);
+  pinMode(DECODER_B, OUTPUT);
+  pinMode(IR_RECEIVER_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
 }
 
 void loop() {
-  // Going forward:
-  leftMotor.run(-motorSpeed); // Negative: wheel turns anti-clockwise
-  rightMotor.run(motorSpeed); // Positive: wheel turns clockwise
-  delay(1000); // Keep going straight for 1000 ms
-  
-  leftMotor.stop(); // Stop left motor
-  rightMotor.stop(); // Stop right motor
-  delay(1000); // Stop for 1000 ms
-  
-  // Turning left (on the spot):
-  leftMotor.run(motorSpeed); // Positive: wheel turns clockwise
-  rightMotor.run(motorSpeed); // Positive: wheel turns clockwise
-  delay(TURNING_TIME_MS); // Keep turning left for this time duration
-  
-  leftMotor.stop(); // Stop left motor
-  rightMotor.stop(); // Stop right motor
-  delay(1000); // Stop for 1000 ms
-  
-  light_show();
+
+  shine_red(RGB_TIME);
+  shine_green(RGB_TIME);
+  shine_blue(RGB_TIME);
+  //    Serial.println(analogRead(IR_RECEIVER_PIN));
+//  Serial.println(analogRead(LDR_PIN));
+  delay(500);
+
+  //  sensorState = lineFinder.readSensors();
+  //
+  //  if (status == 1) {
+  //    int sensorState = lineFinder.readSensors(); //read sensor state
+  //
+  //
+  //    Serial.println(sensorState); //3 if both meeting line
+  //    delay(20); // decision making interval (in milliseconds)
+  //  }
+
+  //  Serial.println(analogRead(IR_RECEIVER));
+  //  delay(1000);
+  //  reverse(300);
 }
